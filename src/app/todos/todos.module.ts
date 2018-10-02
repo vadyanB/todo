7b5d6d6a8531b9b2e2be7a2f50a0c9c83105449c
vadyanB <vadim.b@2muchcoffee.com>
2018-10-02 18:16:11 +0300

    feat HeaderComponent

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { TodosRoutingModule } from './todos-routing.module';
 import { TodosComponent } from './todos.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    TodosRoutingModule
+    TodosRoutingModule,
   ],
-  declarations: [TodosComponent]
+  declarations: [
+    TodosComponent,
+    HeaderComponent
+  ]
 })
 export class TodosModule { }
