2a8b50e8494f751dc21d59b74fcd6858bd2b2521
vadyanB <vadim.b@2muchcoffee.com>
2018-11-13 15:25:58 +0200

    beauty code interceptors, app module

--- src/app/core/interceptors/api-url.interceptor.ts
+++ src/app/core/interceptors/api-url.interceptor.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { environment } from '../../../environments/environment';
 
 @Injectable()
-export class NoopInterceptor implements HttpInterceptor {
+export class ApiUrlInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
